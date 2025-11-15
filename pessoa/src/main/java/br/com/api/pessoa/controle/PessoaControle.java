ff2b237ad039c105eb14a2834b3f06e86b28fc1c
gscode <adm.guilhermedesousa@gmail.com>
2025-11-15 18:06:06 -0300

    implementando rota de listagem GET

--- pessoa/src/main/java/br/com/api/pessoa/controle/PessoaControle.java
+++ pessoa/src/main/java/br/com/api/pessoa/controle/PessoaControle.java
@@ -5,18 +5,17 @@ Implementar a camada de controle, que é responsável por gerenciar rotas.
 Uma rota é um caminho e esse caminho vai executar uma ação (get, post, put, delete).
 */
 
+import br.com.api.pessoa.modelo.PessoaModelo;
 import br.com.api.pessoa.repositorio.PessoaRepositorio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // Aqui estamos especificando que a classe PessoaControle é um controlador REST.
 @RequiredArgsConstructor
 public class PessoaControle {
 
-    // Atributos
+    // Atributo da classe PessoaControle
     // @Autowired // Injeção de dependência
     private final PessoaRepositorio pr; // Com o final estamos aplicando o princípio da imutabilidade
 
@@ -25,14 +24,20 @@ public class PessoaControle {
     //    this.pr = pr;
     //}
 
-    // Metodo
-    @GetMapping("/mensagem") // Mapeia requisições GET para a raiz ("/") do contexto da aplicação.
-    public String mensagem() {
-        return "Aprendendo spring boot v3.5.7"; // Dependência de Live reload para atualizar a página, sem ter que reiniciar o servidor.
+    // Rota responsável pela listagem de pessoas
+    @GetMapping("/")
+    public Iterable<PessoaModelo> listarPessoas() {// Iterable é uma interface de coleções
+        return this.pr.findAll(); // findAll equivale a select * from pessoas
     }
 
-    @GetMapping("/apresentacao/{nome}")
-    public String apresentacao(@PathVariable String nome) {
-        return "Olá " + nome;
-    }
+    // Metodo
+//    @GetMapping("/mensagem") // Mapeia requisições GET para a raiz ("/") do contexto da aplicação.
+//    public String mensagem() {
+//        return "Aprendendo spring boot v3.5.7"; // Dependência de Live reload para atualizar a página, sem ter que reiniciar o servidor.
+//    }
+//
+//    @GetMapping("/apresentacao/{nome}")
+//    public String apresentacao(@PathVariable String nome) {
+//        return "Olá " + nome;
+//    }
 }
