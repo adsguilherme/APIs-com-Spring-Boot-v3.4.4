0579222179cc13f325f3f1f9dcc7cd6efa74a2d8
gscode <adm.guilhermedesousa@gmail.com>
2025-11-29 11:31:44 -0300

    removendo annotations da camada de serviço

--- pessoa/src/main/java/br/com/api/pessoa/servico/PessoaServico.java
+++ pessoa/src/main/java/br/com/api/pessoa/servico/PessoaServico.java
@@ -1,8 +1,15 @@
 package br.com.api.pessoa.servico;
 
+import br.com.api.pessoa.modelo.PessoaModelo;
 import br.com.api.pessoa.repositorio.PessoaRepositorio;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +17,87 @@ public class PessoaServico {
 
   // Atributo da classe
   private final PessoaRepositorio pr;
+
+    // Método responsável pela listagem de pessoas
+    // @GetMapping("/")
+    public ResponseEntity<Iterable<PessoaModelo>> listarPessoas() { // Iterable é uma interface de coleções
+        return new ResponseEntity<>(this.pr.findAll(), HttpStatus.OK); // findAll equivale a select * from pessoas
+    }
+
+    // Método responsável pelo cadastro de pessoas
+    // @PostMapping("/")
+    public ResponseEntity<PessoaModelo> cadastrarPessoa(PessoaModelo pm) { // alterado de @Valid @RequestBody para apenas PessoaModelo pm
+        return new ResponseEntity<>(this.pr.save(pm), HttpStatus.CREATED); // save equivale a into e update
+    }
+
+    // Método responsável pela alteração total dos dados de uma pessoa
+    // @PutMapping("/{codigo}")
+    public ResponseEntity<PessoaModelo> alterarPessoaTotal(Long codigo, PessoaModelo pm) { // @Valid @PathVariable @RequestBody
+        // Obter o registro contido na tabela
+        Optional<PessoaModelo> obj = this.pr.findById(codigo); // select * from pessoas where código = 1
+
+        // Condicional
+        if (obj.isPresent()) {
+            pm.setCodigo(codigo);
+            return new ResponseEntity<>(this.pr.save(pm), HttpStatus.OK);
+        }
+
+        // Caso o ID não exista
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Método responsável pela alteração parcial dos dados
+    // @PatchMapping("/{codigo}")
+    public ResponseEntity<PessoaModelo> alterarPessoaParcial(Long codigo, PessoaModelo pm) { // @PathVariable @RequestBody
+
+        // Forma simplificada não precisando converter Optional para PessoaModelo
+        // PessoaModelo pm2 = this.pr.findById(codigo).get();
+
+        // Obter o registro contido na tabela
+        Optional<PessoaModelo> obj = this.pr.findById(codigo); // select * from pessoas where código = 1
+
+        // Condicional
+        if (obj.isPresent()) {
+            pm.setCodigo(codigo);
+
+            // Converter Optional para PessoaModelo
+            PessoaModelo pm2 = obj.get();
+
+            // Verificação (preciso saber quais as informacoes especificadas no @RequestBody)
+            if (pm.getNome() != null) { // Se pm.getNome for diferente de null
+                pm2.setNome(pm.getNome()); // então pm2.setNome recebe pm.getNome
+            }
+
+            if (pm.getIdade() != null) {
+                pm2.setIdade(pm.getIdade());
+            }
+
+            if (pm.getCidade() != null) {
+                pm2.setCidade(pm.getCidade());
+            }
+
+            // Retorno
+            return new ResponseEntity<>(this.pr.save(pm2), HttpStatus.OK);
+        }
+
+        // Caso o ID não exista
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Método responsável pela remoção dos dados
+    // @DeleteMapping("/{codigo}")
+    public ResponseEntity<Void> removerPessoa(Long codigo) { // alterado de void para a classe Void // @PathVariable
+        // Verificar a existência do id
+        boolean existeCodigo = this.pr.existsById(codigo);
+
+        // Condicional
+        if (existeCodigo) { // Se o existeCodigo for verdadeiro, execute o deleteById e retorne o status
+            this.pr.deleteById(codigo);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        // Caso o ID não exista
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
 
 /**
